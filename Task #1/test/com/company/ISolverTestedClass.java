be58e19fbe57cf197ed8b2c97d657ccb395e0cf2
Martoon <Martoon_00@mail.ru>
2015-02-17 00:35:45 +0300

    e

--- Task #1/test/com/company/ISolverTestedClass.java
+++ Task #1/test/com/company/ISolverTestedClass.java
@@ -5,6 +5,5 @@ import com.company.lang.ISolver;
 
 public class ISolverTestedClass {
     // TODO: select your class here
-
     public static final Class<? extends ISolver> testedClass = JamaSolver.class;
 }
