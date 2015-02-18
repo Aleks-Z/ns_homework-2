670ae975d630cb65e4874187c03b995462c61023
Martoon <Martoon_00@mail.ru>
2015-02-19 01:42:44 +0300

    Tested class set

--- Task #1/test/com/company/ISolverTestedClass.java
+++ Task #1/test/com/company/ISolverTestedClass.java
@@ -1,9 +1,9 @@
 package com.company;
 
-import com.company.conjugateGradient.FletcherReeves;
+import com.company.jamaSolver.JamaSolver;
 import com.company.lang.ISolver;
 
 public class ISolverTestedClass {
     // TODO: select your class here
-    public static final Class<? extends ISolver> testedClass = FletcherReeves.class;
+    public static final Class<? extends ISolver> testedClass = JamaSolver.class;
 }
