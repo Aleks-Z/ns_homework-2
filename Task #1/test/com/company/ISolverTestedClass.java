e98d90babebb6c1a871d0335a65c41bcadb4191b
gelkin <mazin.v.a@gmail.com>
2015-02-21 02:06:23 +0300

    Jacobi method added, not properly tested

--- Task #1/test/com/company/ISolverTestedClass.java
+++ Task #1/test/com/company/ISolverTestedClass.java
@@ -1,9 +1,10 @@
 package com.company;
 
+import com.company.jacobi.Jacobi;
 import com.company.jamaSolver.JamaSolver;
 import com.company.lang.ISolver;
 
 public class ISolverTestedClass {
     // TODO: select your class here
-    public static final Class<? extends ISolver> testedClass = JamaSolver.class;
+    public static final Class<? extends ISolver> testedClass = Jacobi.class;
 }
