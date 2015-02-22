1858bfac3e6e3f068bdd19465cd5fe1f49e11472
Martoon <Martoon_00@mail.ru>
2015-02-22 16:48:41 +0300

    qwer

--- Task #1/test/com/company/ISolverTestedClass.java
+++ Task #1/test/com/company/ISolverTestedClass.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import com.company.jacobi.Jacobi;
 import com.company.jamaSolver.JamaSolver;
 import com.company.lang.ISolver;
+import com.company.seidel.Seidel;
 
 public class ISolverTestedClass {
     // TODO: select your class here
-    public static final Class<? extends ISolver> testedClass = JamaSolver.class;
+    public static final Class<? extends ISolver> testedClass = Seidel.class;
 }
